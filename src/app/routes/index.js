50f4bdb6503c145e73d1a11670c2a313b45dc6f8
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-03-18 13:09:16 -0300

    Concluído

---
+++ src/app/routes/index.js
@@ -0,0 +1,5 @@
+const routes = require('express').Router();
+const get = require('../../controllers/get')
+routes.get( '/', get)
+
+module.exports = routes;
---
+++ src/controllers/get.js
@@ -0,0 +1,12 @@
+module.exports = (req, res) => {
+  res.send(`
+  <html>
+   <head>
+    <meta charset="utf-8"/>
+   </head>
+   <body>
+    <h1>Hello World</h1>
+   </body>
+  </html>
+  `);
+};
